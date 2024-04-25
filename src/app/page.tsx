96ca7d3684a5799f668873ad7cb05e395f49e8e7
gapanovichV <vladgapanovich172@gmail.com>
2024-04-25 23:32:10 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,63 @@
-import React from 'react'
-import Link from 'next/link'
+"use client"
+
+import React, { useState } from "react"
+import clsx from "clsx"
+import ColorThief from "colorthief"
+import DisplayImage from "@/components/DisplayImage"
+import ListItem from "@/components/ListItem"
+import Link from "next/link"
+import { CiSaveUp2 } from "react-icons/ci"
+import styles from "./page.module.scss"
 
 const Home = () => {
+  const [uploadImage, setUploadImage] = useState<string>("")
+  const [colorsPalette, setColorsPalette] = useState<number[][]>([])
+
+  const handleUploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files![0]
+
+    if (file) {
+      const reader = new FileReader()
+      reader.onload = async (event) => {
+        const img = new Image()
+
+        img.onload = () => {
+          const colorThief = new ColorThief()
+          const colorPalette = colorThief.getPalette(img, 6)
+          setColorsPalette(colorPalette)
+          setUploadImage(img.src)
+        }
+        img.src = event.target?.result as string
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+
   return (
-    <ul>
-      <li>
-        <Link href="/color-kidnapper">Color Kidnapper</Link>
-      </li>
-    </ul>
+    <>
+      <header className={clsx(styles.header)}>
+        <h1>Color Kidnapper</h1>
+        <div className={clsx(styles.input)}>
+          <label htmlFor="file">
+            <CiSaveUp2 size="1.25rem" /> Upload Image
+          </label>
+          <input
+            id="file"
+            type="file"
+            accept={"image/*"}
+            hidden
+            onChange={(e) => handleUploadImage(e)}
+          />
+        </div>
+      </header>
+
+      <main className={clsx(styles.main)}>
+        <div className={clsx(styles.content)}>
+          <DisplayImage uploadImage={uploadImage} />
+          <ListItem colorsPalette={colorsPalette} />
+        </div>
+      </main>
+    </>
   )
 }
-
 export default Home
